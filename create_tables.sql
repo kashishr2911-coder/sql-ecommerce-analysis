16b578d7dad729d5af6283c9e61acf4eadf6e8ed
kashishr2911-coder <kashishr2911@gmail.com>
2026-06-24 01:12:57 +0530

    create tables script

---
+++ create_tables.sql
@@ -0,0 +1,34 @@
+CREATE TABLE customers (
+    customer_id INT PRIMARY KEY,
+    customer_name VARCHAR(100),
+    email VARCHAR(100),
+    city VARCHAR(50),
+    signup_date DATE
+);
+
+CREATE TABLE products (
+    product_id INT PRIMARY KEY,
+    product_name VARCHAR(100),
+    category VARCHAR(50),
+    price DECIMAL(10,2)
+);
+
+CREATE TABLE orders (
+    order_id INT PRIMARY KEY,
+    customer_id INT,
+    order_date DATE,
+    total_amount DECIMAL(10,2),
+    FOREIGN KEY (customer_id)
+    REFERENCES customers(customer_id)
+);
+
+CREATE TABLE order_items (
+    item_id INT PRIMARY KEY,
+    order_id INT,
+    product_id INT,
+    quantity INT,
+    FOREIGN KEY (order_id)
+    REFERENCES orders(order_id),
+    FOREIGN KEY (product_id)
+    REFERENCES products(product_id)
+);
